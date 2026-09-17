c715b4b678f8f87f9bd466a88ea77ee553e8c1b1
unknown <vallorasanau@gmaile.com>
2026-09-17 08:28:49 -0500

    Add README and clean up generated files

---
+++ src/constants/config.js
@@ -0,0 +1,12 @@
+// Central configuration for FMP API.
+//
+const config = {
+  FMP_BASE_URL: 'https://financialmodelingprep.com/stable',
+  FMP_API_KEY: 'YOUR_API_KEY_HERE',
+  FINANCIAL_YEARS: 5,
+};
+
+export default config;
+
+
+ 
